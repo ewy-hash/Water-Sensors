1075b3a18d323f944353dbe1fb90d12cbec58eda
ewy-hash <ewyers@students.pitzer.edu>
2025-11-12 20:18:52 -0800

    Starting to make date Object

--- Initial Script.R
+++ Initial Script.R
@@ -1,4 +1,9 @@
 ##HIIIIII 
+install.packages("pak")
+library(pak)
+pak::pak("hadley/genzplyr")library()
 library(tidyverse)
-WaterData <- read_csv(file = "Water BKT 3 data.csv")
-WaterData$...5 = NULL
+WaterData <- read_csv(file = "Water-BKT-3-data.csv")
+
+WaterData$DateTime <- as.Date(x = WaterData$DATE + WaterData$TIME, format)
+tempPlot <- ggplot(data = WaterData, mapping = aes())
